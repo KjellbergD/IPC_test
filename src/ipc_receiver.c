68b95d246db3c8bb0b4ec90ecf266af80f9122bb
Daniel <dakj@itu.dk>
2024-04-09 15:54:11 +0200

    Simple testing + timing finished
    Co-authored-by: Nikolaj Sørensen <nikso@itu.dk>
    Co-authored-by: Jeppe Lindhard <jepli@itu.dk>

--- src/ipc_receiver.c
+++ src/ipc_receiver.c
@@ -1,66 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
 #include <time.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
 #define PIPE_PATH "pipe"
-#define BILLION 1000000000L  // 1 billion nanoseconds in a second
+#define PIPE_CHUNK_SIZE (1024 * 64)
+#define MSGQ_CHUNK_SIZE (1024 * 8)
+#define SHM_KEY 200
+#define MSGQ_KEY 73
+#define BILLION 1000000000L // 1 billion nanoseconds in a second
 
 // Function declarations
 void shared_memory();
 void pipes();
 void msg_queue();
+void save_image(unsigned char *image_data);
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Usage: %s <function_name>\n", argv[0]);
-        return 1;
+        return -1;
     }
 
     // Check the string argument and call respective functions
-    if (strcmp(argv[1], "shared") == 0) {
+    if (strcmp(argv[1], "shared") == 0)
+    {
         shared_memory();
-    } else if (strcmp(argv[1], "pipes") == 0) {
+    }
+    else if (strcmp(argv[1], "pipes") == 0)
+    {
         pipes();
-    } else if (strcmp(argv[1], "queue") == 0) {
+    }
+    else if (strcmp(argv[1], "queue") == 0)
+    {
         msg_queue();
-    } else {
+    }
+    else
+    {
         printf("Invalid function name: %s. Valid names are 'shared', 'pipes' and 'queue'\n", argv[1]);
-        return 1;
+        return -1;
     }
 
     return 0;
 }
 
 // Experiment for Shared Memory
-void shared_memory() {
-    printf("This is function A.\n");
+void shared_memory()
+{
+    struct timespec before_attach_time, after_attach_time;
+
+    // Recieve shared memory id from recieved data
+    int shmid;
+
+    // Start timer before attaching to shared memory segment
+    if (clock_gettime(CLOCK_MONOTONIC, &before_attach_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((shmid = shmget(SHM_KEY, 0, 0)) == -1)
+    {
+        perror("Shared memory get");
+        return;
+    }
+
+    // Attach to shared memory from id
+    void *shmaddr = shmat(shmid, NULL, 0);
+
+    if (shmaddr == (void *)-1)
+    {
+        perror("Shared memory attach");
+        return;
+    }
+
+    // End timer after attaching to shared memory
+    if (clock_gettime(CLOCK_MONOTONIC, &after_attach_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    // Calculate time it took to attach to shared memory segment
+    long elapsed_time_ns = ((after_attach_time.tv_sec - before_attach_time.tv_sec) \
+            * BILLION) + (after_attach_time.tv_nsec - before_attach_time.tv_nsec);
+
+    printf("%ld", elapsed_time_ns);
+
+    save_image(shmaddr);
+
+    // Cleanup: Detach from and delete the shared memory segment
+    shmdt(shmaddr);
+    shmctl(shmid, IPC_RMID, NULL);
 }
 
 // Experiment for Pipe communication
-void pipes() {
-    char buffer[100];
+void pipes()
+{
     int fd;
     struct timespec receive_time;
 
     // Create or open the FIFO (named pipe) for reading
-    if ((fd = open(PIPE_PATH, O_RDONLY)) < 0) {
+    if ((fd = open(PIPE_PATH, O_RDONLY)) < 0)
+    {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
     // Read message from the FIFO
-    read(fd, buffer, sizeof(buffer));
+    int image_size;
+    read(fd, &image_size, sizeof(int));
+    unsigned char buffer[image_size];
+
+    size_t total_read = 0;
+
+    while (total_read < image_size)
+    {
+        size_t remaining = image_size - total_read;
+        size_t read_size = remaining < PIPE_CHUNK_SIZE ? remaining : PIPE_CHUNK_SIZE;
+
+        read(fd, buffer + total_read, read_size);
+
+        total_read += read_size;
+    }
 
     // Get the end time
-    if (clock_gettime(CLOCK_MONOTONIC, &receive_time) < 0) {
+    if (clock_gettime(CLOCK_MONOTONIC, &receive_time) < 0)
+    {
         perror("clock_gettime");
         exit(EXIT_FAILURE);
     }
 
     printf("%ld", BILLION * receive_time.tv_sec + receive_time.tv_nsec);
+
+    save_image(buffer);
     // printf("Recieve time was: %llu \n", receive_time.tv_nsec);
 
     // printf("Receiver received message: %s\n", buffer);
@@ -69,7 +152,89 @@ void pipes() {
     close(fd);
 }
 
+// Message Queue Struct
+typedef struct ImageData
+{
+    long mtype;
+    unsigned char data[MSGQ_CHUNK_SIZE];
+} ImageData;
+
+typedef struct DataSize
+{
+    long mtype;
+    int size;
+} DataSize;
+
 // Experiment for message queue communication
-void msg_queue() {
-    printf("This is function C.\n");
+void msg_queue()
+{
+    int msqid;
+    struct DataSize data_size;
+    struct ImageData receive_data;
+
+    struct timespec receive_time;
+
+    // Access the message queue
+    if ((msqid = msgget(MSGQ_KEY, IPC_CREAT | 0666)) < 0)
+    {
+        perror("msgget receiver");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive message from the queue
+    if (msgrcv(msqid, &data_size, sizeof(data_size.size), 1, 0) < 0)
+    {
+        perror("msgrcv receiver 1");
+        exit(EXIT_FAILURE);
+    }
+
+    // Fetch size of image data
+    int image_size = data_size.size;
+    unsigned char *image_data = (unsigned char *)malloc(image_size);
+
+    size_t total_read = 0;
+
+    // Recieve image data through multiple messages
+    while (total_read < image_size)
+    {
+        size_t remaining = image_size - total_read;
+        size_t read_size = remaining < MSGQ_CHUNK_SIZE ? remaining : MSGQ_CHUNK_SIZE;
+
+        // Receive message from the queue
+        if (msgrcv(msqid, &receive_data, MSGQ_CHUNK_SIZE, 1, 0) < 0)
+        {
+            perror("msgrcv receiver 2");
+            exit(EXIT_FAILURE);
+        }
+
+        // Append the read image chunk onto the buffer which holds all the image data
+        memcpy(image_data + total_read, receive_data.data, read_size);
+
+        total_read += read_size;
+    }
+
+    // Get the end time
+    if (clock_gettime(CLOCK_MONOTONIC, &receive_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%ld", BILLION * receive_time.tv_sec + receive_time.tv_nsec);
+
+    save_image(image_data);
+}
+
+void save_image(unsigned char *image_data)
+{
+    int image_width = *(int *)image_data;
+    int image_height = *((int *)(image_data + sizeof(int)));
+    int image_channels = *((int *)(image_data + sizeof(int) * 2));
+
+    int stride = image_width * image_channels * sizeof(uint8_t);
+    int success = stbi_write_png("output.png", image_width, image_height, image_channels, image_data + sizeof(int) * 3, stride);
+    if (!success)
+    {
+        fprintf(stderr, "Error writing image\n");
+    }
 }
--- src/ipc_sender.c
+++ src/ipc_sender.c
@@ -4,73 +4,210 @@
 #include <string.h>
 #include <fcntl.h>
 #include <time.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #define PIPE_PATH "pipe"
-#define BILLION 1000000000L  // 1 billion nanoseconds in a second
+#define PIPE_CHUNK_SIZE (1024 * 64)
+#define MSGQ_CHUNK_SIZE (1024 * 8)
+#define SHM_KEY 200
+#define MSGQ_KEY 73
+#define BILLION 1000000000L // 1 billion nanoseconds in a second
+#define TEST_IMG "img/25KB.png"
+
 
 // Function declarations
-void shared_memory();
-void pipes();
-void msg_queue();
+void shared_memory(unsigned char *image_data, int image_size);
+void pipes(unsigned char *image_data, int image_size);
+void msg_queue(unsigned char *image_data, int image_size);
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         printf("Usage: %s <function_name>\n", argv[0]);
-        return 1;
+        return -1;
     }
 
+    // Load test image
+    int image_width, image_height, image_channels;
+    unsigned char *raw_image_data = stbi_load(TEST_IMG, &image_width, &image_height, &image_channels, STBI_rgb_alpha);
+
+    // Insert image metadata
+    int raw_image_size = image_width * image_height * image_channels;
+    int image_data_size = raw_image_size + 3 * sizeof(int);
+    unsigned char *image_data = (unsigned char *)malloc(image_data_size);
+    memcpy(image_data + sizeof(int) * 0, &image_width, sizeof(int));
+    memcpy(image_data + sizeof(int) * 1, &image_height, sizeof(int));
+    memcpy(image_data + sizeof(int) * 2, &image_channels, sizeof(int));
+    memcpy(image_data + sizeof(int) * 3, raw_image_data, raw_image_size);
+    stbi_image_free(raw_image_data);
+
     // Check the string argument and call respective functions
-    if (strcmp(argv[1], "shared") == 0) {
-        shared_memory();
-    } else if (strcmp(argv[1], "pipes") == 0) {
-        pipes();
-    } else if (strcmp(argv[1], "queue") == 0) {
-        msg_queue();
-    } else {
+    if (strcmp(argv[1], "shared") == 0)
+    {
+        shared_memory(image_data, image_data_size);
+    }
+    else if (strcmp(argv[1], "pipes") == 0)
+    {
+        pipes(image_data, image_data_size);
+    }
+    else if (strcmp(argv[1], "queue") == 0)
+    {
+        msg_queue(image_data, image_data_size);
+    }
+    else
+    {
         printf("Invalid function name: %s. Valid names are 'shared', 'pipes' and 'queue'\n", argv[1]);
-        return 1;
+        return -1;
     }
 
     return 0;
 }
 
 // Experiment for Shared Memory
-void shared_memory() {
-    printf("This is function A.\n");
+void shared_memory(unsigned char *image_data, int image_size)
+{
+    struct timespec start_time, end_time;
+    
+    // Get the start time
+    if (clock_gettime(CLOCK_MONOTONIC, &start_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+    
+    int shmid = shmget(SHM_KEY, image_size, IPC_CREAT | 0666);
+    char *shmaddr = shmat(shmid, NULL, 0);
+
+    // Copy image data to shared memory segment
+    memcpy(shmaddr, image_data, image_size);
+
+    // Get the end time
+    if (clock_gettime(CLOCK_MONOTONIC, &end_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    // Calculate time it took to create, attach and copy to shared memory
+    long elapsed_time_ns = (end_time.tv_sec - start_time.tv_sec) \
+            * BILLION + (end_time.tv_nsec - start_time.tv_nsec);
+
+    printf("%ld", elapsed_time_ns);
+
+    // Detach from shared memory
+    shmdt(shmaddr);
 }
 
 // Experiment for Pipe communication
-void pipes() {
-    char message[] = "Hello from sender!";
+void pipes(unsigned char *image_data, int image_size)
+{
     int fd;
     struct timespec send_time;
 
     // Create or open the FIFO (named pipe) for writing
-    if ((fd = open(PIPE_PATH, O_WRONLY)) < 0) {
+    if ((fd = open(PIPE_PATH, O_WRONLY)) < 0)
+    {
         perror("open");
         exit(EXIT_FAILURE);
     }
 
     // Get the start time
-    if (clock_gettime(CLOCK_MONOTONIC, &send_time) < 0) {
+    if (clock_gettime(CLOCK_MONOTONIC, &send_time) < 0)
+    {
         perror("clock_gettime");
         exit(EXIT_FAILURE);
     }
-    
+
     printf("%ld", BILLION * send_time.tv_sec + send_time.tv_nsec);
     // printf("Send time was: %llu \n", send_time.tv_nsec);
 
     // Write message to the FIFO
-    write(fd, message, strlen(message) + 1);
+    write(fd, &image_size, sizeof(int));
+
+    size_t total_sent = 0;
+
+    while (total_sent < image_size)
+    {
+        size_t remaining = image_size - total_sent;
+        size_t send_size = remaining < PIPE_CHUNK_SIZE ? remaining : PIPE_CHUNK_SIZE;
+
+        write(fd, image_data + total_sent, send_size);
+
+        total_sent += send_size;
+    }
+
     // printf("Sender sent message: %s\n", message);
 
     // Close the FIFO
     close(fd);
 }
 
+// Message Queue Struct
+typedef struct ImageData
+{
+    long mtype;
+    unsigned char data[MSGQ_CHUNK_SIZE];
+} ImageData;
+
+typedef struct DataSize
+{
+    long mtype;
+    int size;
+} DataSize;
+
 // Experiment for message queue communication
-void msg_queue() {
-    printf("This is function C.\n");
+void msg_queue(unsigned char *image_data, int image_size)
+{
+    struct timespec send_time;
+
+    // Get the start time
+    if (clock_gettime(CLOCK_MONOTONIC, &send_time) < 0)
+    {
+        perror("clock_gettime");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("%ld", BILLION * send_time.tv_sec + send_time.tv_nsec);
+    
+    // Create or access the message queue
+    int msqid;
+    if ((msqid = msgget(MSGQ_KEY, IPC_CREAT | 0666)) < 0) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+    
+    DataSize data_size;
+    data_size.mtype = 1;
+    data_size.size = image_size;
+
+    // Send message to the queue
+    if (msgsnd(msqid, &data_size, sizeof(int), 0) < 0) {
+        perror("msgsnd");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t total_sent = 0;
+
+    // Send image data in multple messages
+    while (total_sent < image_size)
+    {
+        size_t remaining = image_size - total_sent;
+        size_t send_size = remaining < MSGQ_CHUNK_SIZE ? remaining : MSGQ_CHUNK_SIZE;
+
+        ImageData send_data;
+        send_data.mtype = 1;
+        memcpy(send_data.data, image_data + total_sent, send_size);
+
+        // Send message to the queue
+        if (msgsnd(msqid, &send_data, MSGQ_CHUNK_SIZE, 0) < 0) {
+            perror("msgsnd");
+            exit(EXIT_FAILURE);
+        }
+
+        total_sent += send_size;
+    }
 }
